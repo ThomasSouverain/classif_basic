d481f5ce220739367afc267305c5b853275f39ff
thomas souverain <thomas.souverain@dreamquark.com>
2023-02-01 00:23:12 +0100

    feat: option to handle categorical features in data prep

--- classif_basic/data_preparation.py
+++ classif_basic/data_preparation.py
@@ -11,11 +11,52 @@ from sklearn import ensemble
 from sklearn.metrics import log_loss
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from xgboost import XGBClassifier
 from xgboost import XGBRegressor
 
 # General data preparations
-def train_valid_test_split(X: pd.DataFrame, Y: pd.DataFrame, model_task: str) -> pd.DataFrame:
+def handle_cat_features(X: pd.DataFrame, preprocessing_cat_features: str) -> pd.DataFrame:
+    """Returns the initial DataFrame with the categorical features handled for machine-learning computation.
+    Args:
+        X : pd.DataFrame
+            DataFrame with all features (entered as columns) concerning individuals
+        preprocessing_cat_features: str
+            Set the way categorial features are handled. 
+            Keep unique columns and replace their values by numbers ("label_encoding"), or create one column per feature's value ("one_hot_encoding").
+            Must be set to a value in {"label_encoding", "one_hot_encoding"}
+    Raises:
+        NotImplementedError:
+            When the way the user wants to handle categorial features is not implemented. Must be set to a value in {'label_encoding', 'one_hot_encoding'}
+    Returns:
+        pd.DataFrame: the initial DataFrame with the categorical features handled for machine-learning computation.
+    """
+
+    if preprocessing_cat_features == "label_encoding":
+
+        X_category = X.loc[:, X.dtypes == "category"]
+
+        le = LabelEncoder()
+        X_category = X_category.apply(le.fit_transform)
+
+        # for further interpretation, to get back the original categories
+        # X_category.apply(le.inverse_transform)
+
+        # then, reconstitute the dataset with the label-encoded categorial columns 
+        X_non_category = X.loc[:, X.dtypes != "category"]
+
+        X = pd.concat([X_non_category, X_category], axis=1)
+
+    elif preprocessing_cat_features == "one_hot_encoding":
+        X = pd.get_dummies(X)
+
+    else:
+        raise NotImplementedError("The way you want to handle categorial features is not implemented. Must be set to a value in {'label_encoding', 'one_hot_encoding'}")
+
+    return X
+
+# General data preparations
+def train_valid_test_split(X: pd.DataFrame, Y: pd.DataFrame, model_task: str, preprocessing_cat_features: str = "one_hot_encoding") -> pd.DataFrame:
     """Splits data into train, valid, and test set -> to train a model with cross-validation
     & train_valid set -> to inspect if the model is discriminant on the data it trained
 
@@ -28,6 +69,10 @@ def train_valid_test_split(X: pd.DataFrame, Y: pd.DataFrame, model_task: str) ->
     model_task: str
         Goal the user wants to achieve with the model: either classify, or regress...
         Must be set to value in {"regression", "classification"}
+    preprocessing_cat_features: str
+        Set the way categorial features are handled. 
+        Keep unique columns and replace their values by numbers ("label_encoding"), or create one column per feature's value ("one_hot_encoding").
+        Must be set to a value in {"label_encoding", "one_hot_encoding"}
 
     Returns
     -------
@@ -40,9 +85,10 @@ def train_valid_test_split(X: pd.DataFrame, Y: pd.DataFrame, model_task: str) ->
     SEED = 7
     VALID_SIZE = 0.15
 
+    X = handle_cat_features(X=X, preprocessing_cat_features=preprocessing_cat_features)
+
     if model_task in {"classification", "multiclass"}:
 
-        X = pd.get_dummies(X)
         # Keep test values to ensure model is behaving properly
         X_model, X_test, Y_model, Y_test = train_test_split(
             X, Y, test_size=VALID_SIZE, random_state=SEED, stratify=Y
@@ -59,7 +105,6 @@ def train_valid_test_split(X: pd.DataFrame, Y: pd.DataFrame, model_task: str) ->
 
     elif model_task == "regression":
 
-        X = pd.get_dummies(X)
         # Keep test values to ensure model is behaving properly
         X_model, X_test, Y_model, Y_test = train_test_split(
             X,
